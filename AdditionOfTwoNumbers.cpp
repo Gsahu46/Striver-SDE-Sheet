337001862d4b44514a49c8d51ba416ec527d78ca
Gsahu46 <gsahu4900@gmail.com>
2024-01-25 10:22:15 +0530

    This is the basic code of addition for testing

---
+++ AdditionOfTwoNumbers.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "This is testing code";
+    int n, m;
+
+    cin >> n >> m;
+
+    int result = n + m;
+    cout << result << endl;
+
+    return 0;
+} 
